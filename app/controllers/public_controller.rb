b2166b85530548d7839a217c6742c073026ae9fa
Justin Adkins <justin.adkins@mythcoders.com>
2019-04-20 23:19:20 -0400

    FAQ

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -6,8 +6,18 @@ class PublicController < ApplicationController
     @workshop = Workshop.find(params[:workshop_id])
   end
 
+  def faq
+    @questions = YAML.load_file(faq_file_path)
+  end
+
   def my_account
     # @upcoming_tickets = current_user.tickets.select { |t| t.workshop.start_date.future? }
     # @past_tickets = current_user.tickets.select { |t| !t.workshop.start_date.future? }
+  end
+
+  private
+
+  def faq_file_path
+    Rails.root.join('lib', 'faq.yaml')
   end
 end
