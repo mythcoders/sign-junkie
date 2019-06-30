0d705e2f62374b3f676eb8ab52b5caf2cc57a059
Justin Adkins <justin.adkins@mythcoders.com>
2019-06-30 05:14:53 +0000

    v1806.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
 
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_controller.asset_host = 'https://signjunkieworkshop.com'
+  config.action_controller.asset_host = 'https://www.signjunkieworkshop.com'
 
   config.require_master_key = true
 
@@ -23,7 +23,10 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.asset_host = 'https://signjunkieworkshop.com'
+  config.action_mailer.asset_host = 'https://www.signjunkieworkshop.com'
+  config.action_mailer.default_url_options = {
+    host: 'https://www.signjunkieworkshop.com'
+  }
 
   # config.action_mailer.raise_delivery_errors = false
 
