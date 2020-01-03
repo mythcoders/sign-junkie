42ca760e2bbb6ad0068eb0525d298c98be4609c0
Justin Adkins <justin.adkins@mythcoders.com>
2020-01-02 23:20:57 -0500

    Small changes

--- app/reports/sales_tax_report.rb
+++ app/reports/sales_tax_report.rb
@@ -14,26 +14,34 @@ class SalesTaxReport
   end
 
   def tax_collected
-    (data.map(&:tax_amount).reduce(:+) || 0.00).round(2)
+    (raw_data.map(&:tax_amount).reduce(:+) || 0.00).round(2)
   end
 
   def taxes_paid
     0.00
   end
 
+  def total_sales
+    taxable_sales + nontaxable_sales
+  end
+
   def taxable_sales
-    (data.map(&:taxable_amount).reduce(:+) || 0.00).round(2)
+    (raw_data.map(&:taxable_amount).reduce(:+) || 0.00).round(2)
   end
 
   def nontaxable_sales
-    (data.map(&:nontaxable_amount).reduce(:+) || 0.00).round(2)
+    (raw_data.map(&:nontaxable_amount).reduce(:+) || 0.00).round(2)
+  end
+
+  def raw_data
+    return nil if tax_period.nil?
+
+    @raw_data ||= purchased_items
   end
 
   private
 
-  def data
-    return nil if tax_period.nil?
-
+  def purchased_items
     ItemDescription
       .joins(:invoice_items)
       .where('invoice_items.created_at BETWEEN ? and ?', start_date, end_date)
