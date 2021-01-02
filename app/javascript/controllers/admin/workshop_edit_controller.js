895d011bfd2ebbad0bc04c59b566461b7aae595c
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-02 12:42:44 -0500

    Workshop options

--- app/javascript/controllers/admin/workshop_edit_controller.js
+++ app/javascript/controllers/admin/workshop_edit_controller.js
@@ -5,14 +5,14 @@ export default class extends AdminController {
     "reservationAllowMultiple", "totalSeats", "reservationPrice", "reservationMinimum", "reservationMaximum"]
 
   toggleType(e) {
-    this.singleSeatAllowTarget.innerHTML = e.currentTarget.dataset.singleSeatAllow
+    this.reservationAllowMultipleTarget.innerHTML = e.currentTarget.dataset.reservationAllowMultiple
     this.reservationAllowTarget.innerHTML = e.currentTarget.dataset.reservationAllow
     this.reservationCancelMinimumTarget.innerHTML = e.currentTarget.dataset.reservationCancelMinimum
-    this.reservationsEndTarget.innerHTML = e.currentTarget.dataset.reservationsEnd
-    this.reservationAllowMultipleTarget.innerHTML = e.currentTarget.dataset.reservationAllowMultiple
-    this.totalSeatsTarget.innerHTML = e.currentTarget.dataset.totalSeats
-    this.reservationPriceTarget.innerHTML = e.currentTarget.dataset.reservationPrice
-    this.reservationMinimumTarget.innerHTML = e.currentTarget.dataset.reservationMinimum
     this.reservationMaximumTarget.innerHTML = e.currentTarget.dataset.reservationMaximum
+    this.reservationMinimumTarget.innerHTML = e.currentTarget.dataset.reservationMinimum
+    this.reservationPriceTarget.innerHTML = e.currentTarget.dataset.reservationPrice
+    this.reservationsEndTarget.innerHTML = e.currentTarget.dataset.reservationsEnd
+    this.singleSeatAllowTarget.innerHTML = e.currentTarget.dataset.singleSeatAllow
+    this.totalSeatsTarget.innerHTML = e.currentTarget.dataset.totalSeats
   }
 }
