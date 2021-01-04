05e1ac76da895d45fa0d36defb01f2c84944fcf4
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-04 17:21:54 -0500

    NavLink

---
+++ app/components/seat_wizard_nav_link/component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module SeatWizardNavLink
+  class Component < ViewComponent::Base
+    def initialize(name, disabled:, active:)
+      @name = name
+      @disabled = disabled
+    end
+
+    with_content_areas :body
+
+    def css_class
+      css = 'nav-link'
+      css += ' disabled' if @disabled
+      css += ' active' if @active
+      css
+    end
+  end
+end
