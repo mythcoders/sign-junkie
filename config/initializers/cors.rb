77ebe8582b2812c0340ab2fb37324455ba2cae0b
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-15 01:55:10 -0500

    CORS change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
   allow do
     origins '*'
 
-    resource '/assets/*', headers: :any, methods: [:get]
-    resource '/packs/*', headers: :any, methods: [:get]
+    resource '/assets/*', headers: :any, methods: %i[get options]
+    resource '/packs/*', headers: :any, methods: %i[get options]
   end
 end
