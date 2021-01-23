dd8731d9d4e4a63d4e70ad77e7d1ddafe507f25d
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-23 13:52:44 -0500

    Show stencils view

--- app/controllers/stencils_controller.rb
+++ app/controllers/stencils_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class StencilsController < ApplicationController
+  before_action :set_stencil, only: %i[show]
+
   def index
     @categories = StencilCategory.includes(stencils: [{ image_attachment: :blob }])
+  end
+
+  private
+
+  def set_stencil
+    @stencil = Stencil.find params[:id]
   end
 end
