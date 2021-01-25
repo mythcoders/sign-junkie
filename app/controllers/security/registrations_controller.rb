9596ec845cb7831e60fc7232f2a2c38ff2d54563
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-25 15:58:16 -0500

    Recaptcha fixes

--- app/controllers/security/registrations_controller.rb
+++ app/controllers/security/registrations_controller.rb
@@ -2,23 +2,21 @@
 
 module Security
   class RegistrationsController < Devise::RegistrationsController
-    # include Recaptcha::Adapters::ControllerMethods
-    # include Recaptcha::Adapters::ViewMethods
-
     def create
-      if verify_recaptcha(model: resource)
+      self.resource = resource_class.new sign_up_params
+
+      if verify_recaptcha(model: resource, action: 'registrations_new')
         super
       else
-        recaptcha_error
+        my_recaptcha_error
       end
     end
 
     private
 
-    def recaptcha_error
-      self.resource = resource_class.new sign_up_params
+    def my_recaptcha_error
       resource.validate
-      resource.errors.add(:base, "Registration Error. Please try again")
+      resource.errors.add(:base, 'Registration Error. Please try again')
       # this part is different from the devise wiki, since we don't use a create template, only new
       respond_with_navigational(resource) { render :new }
     end
