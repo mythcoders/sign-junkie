915bc775c60243d56bbab19d78533ffca29a77c4
Justin Adkins <justin.adkins@mythcoders.com>
2021-01-31 22:03:01 -0500

    Use hermes everywhere

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
