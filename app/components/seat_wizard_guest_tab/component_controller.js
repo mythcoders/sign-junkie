fd4101d8e1c04f565dc5c255ea865be45c9cf76c
Justin Adkins <justin.adkins@mythcoders.com>
2021-02-08 00:18:59 -0500

    GitLab CI changes (#75)
    * Color changes
    
    * Remove namespace names

--- app/components/seat_wizard_guest_tab/component_controller.js
+++ app/components/seat_wizard_guest_tab/component_controller.js
@@ -47,7 +47,7 @@ export default class extends ApplicationController {
   toggleGuestType(e) {
     this.guestTypeValue = e.currentTarget.value
 
-    if (this.forReservationValue && this.guestTypeValue !== 'other') {
+    if (this.forReservationValue && this.guestTypeValue === 'adult') {
       this.purchaseModeValue = 'later'
     } else {
       this.purchaseModeValue = 'now'
