f9c1c03838fa30d8272aaa02e6e88400b8d853b4
Justin Adkins <justin.adkins@mythcoders.com>
2021-03-27 17:39:12 -0400

    Fix seat void worker

--- app/workers/seat_void_worker.rb
+++ app/workers/seat_void_worker.rb
@@ -8,7 +8,7 @@ class SeatVoidWorker
     return if seat.voided?
 
     Rails.logger.info "SeatVoidWorker #{seat_id}"
-    SeatService::Void.call(seat)
+    SeatService::Void.perform(seat)
     SeatMailer.with(seat_id: seat.id).voided.deliver_later
   end
 end
