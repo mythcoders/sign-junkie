6032245ab6f0122cfcf430ae7c820c2facf84adb
Justin Adkins <justin.adkins@mythcoders.com>
2022-04-09 11:07:32 -0400

    Adjust seed values

--- db/seeds/optional/30-crafts.rb
+++ db/seeds/optional/30-crafts.rb
@@ -1,3 +1,4 @@
+
 # frozen_string_literal: true
 
 puts "Creating stencils categories..."
@@ -7,26 +8,33 @@ end
 
 puts "Creating stencils..."
 6.times do
-  stencil = Stencil.create(name: Faker::Movies::StarWars.quote,
-    category: StencilCategory.all.sample)
+  stencil = Stencil.create(
+    name: "#{Faker::Movies::StarWars.quote} #{Faker::Lorem.characters(number: 10)}",
+    category: StencilCategory.all.sample
+  )
   stencil.image.attach(new_image)
 end
 
 puts "Creating addon..."
 
 3.times do
-  addon = Addon.create(name: "#{Faker::Commerce.material} #{Faker::House.room} #{Faker::House.furniture}",
-    price: Faker::Commerce.price)
+  addon = Addon.create(
+    name: "#{Faker::Commerce.material} #{Faker::House.room} #{Faker::House.furniture}",
+    price: Faker::Commerce.price
+  )
   addon.addon_images.attach([new_image, new_image])
 end
 
 puts "Creating project..."
 
 6.times do
-  project = Project.create(name: Faker::Commerce.product_name,
+  project = Project.create(
+    name: Faker::Commerce.product_name,
     material_price: Faker::Commerce.price,
     instructional_price: Faker::Commerce.price,
     addons: Addon.all.sample(2),
-    stencils: Stencil.all.sample(4))
+    stencils: Stencil.all.sample(4)
+  )
+
   project.project_images.attach([new_image, new_image])
 end
